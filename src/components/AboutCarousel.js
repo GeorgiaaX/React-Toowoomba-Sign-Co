e3b9163fce0c6e3d1dc499cf5a253a985fce08c1
GeorgiaaX <georgiaa.devv@gmail.com>
2023-10-30 12:49:29 +1000

    image changes

--- src/components/AboutCarousel.js
+++ src/components/AboutCarousel.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Carousel from 'react-bootstrap/Carousel';
 
 function AboutCarousel() {
+
+  useEffect(() => {
+    const imageUrls = [
+      "../images/kfc.jpg",
+      "../images/dominoes.jpg",
+      "../images/bayplaza.jpg"
+    ];
+
+    imageUrls.forEach(src => {
+      const img = new Image();
+      img.src = src;
+    });
+
+  }, []); 
+
   return (
     <div className="about-carousel-container">
       <Carousel prevLabel="" nextLabel="" indicators={false}>
@@ -10,7 +25,7 @@ function AboutCarousel() {
             className="d-block w-100"
             src="../images/kfc.jpg"
             alt="First slide"
-            loading = "lazy"
+            loading="lazy"
           />
         </Carousel.Item>
         <Carousel.Item>
@@ -18,8 +33,7 @@ function AboutCarousel() {
             className="d-block w-100"
             src="../images/dominoes.jpg"
             alt="Second slide"
-            loading = "lazy"
-
+            loading="lazy"
           />
         </Carousel.Item>
         <Carousel.Item>
@@ -27,8 +41,7 @@ function AboutCarousel() {
             className="d-block w-100"
             src="../images/bayplaza.jpg"
             alt="Third slide"
-            loading = "lazy"
-
+            loading="lazy"
           />
         </Carousel.Item>
       </Carousel>
