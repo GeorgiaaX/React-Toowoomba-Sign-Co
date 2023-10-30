219c80fef6132209e58a821bb34dd5a34fd77ce4
GeorgiaaX <georgiaa.devv@gmail.com>
2023-10-31 09:48:22 +1000

    page reload

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
-import Home from './pages/Home'
-import Contact from './pages/Contact'
-import Product from './pages/Product'
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import Product from './pages/Product';
 
 function App() {
+  useEffect(() => {
+    const handleBackButtonEvent = (event) => {
+      window.location.reload(true);
+    };
+
+    window.addEventListener('popstate', handleBackButtonEvent);
+
+    return () => {
+      // Cleanup the event listener on component unmount
+      window.removeEventListener('popstate', handleBackButtonEvent);
+    };
+  }, []);
+
   return (
     <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/home" element = {<Home />} />
-            <Route path="/contact" element = {<Contact />} />
-            <Route path="/product/:productId" element = {<Product />} />
-
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/product/:productId" element={<Product />} />
+      </Routes>
     </BrowserRouter>
   );
 }
