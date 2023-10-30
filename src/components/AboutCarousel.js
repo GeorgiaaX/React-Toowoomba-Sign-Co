fbcb22a5a37b8a06f247ba6f00e093bce90c2a16
GeorgiaaX <georgiaa.devv@gmail.com>
2023-10-30 13:21:56 +1000

    path changes

--- src/components/AboutCarousel.js
+++ src/components/AboutCarousel.js
@@ -1,47 +1,30 @@
-import React, { useEffect } from 'react'
+import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 
+
 function AboutCarousel() {
-
-  useEffect(() => {
-    const imageUrls = [
-      "../images/kfc.jpg",
-      "../images/dominoes.jpg",
-      "../images/bayplaza.jpg"
-    ];
-
-    imageUrls.forEach(src => {
-      const img = new Image();
-      img.src = src;
-    });
-
-  }, []); 
-
   return (
     <div className="about-carousel-container">
-      <Carousel prevLabel="" nextLabel="" indicators={false}>
+      <Carousel prevLabel="" nextLabel="" indicators={false} fade>
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="../images/kfc.jpg"
+            src='./images/kfc.jpg'
             alt="First slide"
-            loading="lazy"
           />
         </Carousel.Item>
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="../images/dominoes.jpg"
+            src='./images/dominoes.jpg'
             alt="Second slide"
-            loading="lazy"
           />
         </Carousel.Item>
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="../images/bayplaza.jpg"
+            src='./images/bayplaza.jpg'
             alt="Third slide"
-            loading="lazy"
           />
         </Carousel.Item>
       </Carousel>
